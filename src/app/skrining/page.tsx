9b25385d2cb35c5e612b5d07a0df9f96cb437cd6
wildandr <wildandzakyramadhani@mail.com>
2025-07-05 21:29:01 +0700

    fix error

--- src/app/skrining/page.tsx
+++ src/app/skrining/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-// import Link from 'next/link';
 
 export default function Skrining() {
   const [age, setAge] = useState<string>('');
--- src/app/skrining/under-55/page.tsx
+++ src/app/skrining/under-55/page.tsx
@@ -8,7 +8,7 @@ export default function Under55Questionnaire() {
     <Questionnaire
       questions={questionsUnder55}
       title="Skrining untuk Usia 18-54 Tahun"
-      subtitle="Silakan jawab semua pertanyaan sesuai dengan perasaan dan pengalamanmu selama 2 minggu terakhir."
+      subtitle="Silakan jawab semua pertanyaan sesuai dengan perasaan dan pengalaman Anda selama 2 minggu terakhir."
       ageGroup="under-55"
     />
   );
